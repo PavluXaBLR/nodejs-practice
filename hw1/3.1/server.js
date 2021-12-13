193a11c66653771e42c9e21c7794a5974615e213
PavluXaBLR <pavel.ulasik1984@gmail.com>
2021-12-13 09:16:28 +0300

    divided by tasks

---
+++ hw1/3.1/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const fs = require ('fs');
+
+
+const server = http.createServer(function(request, response) {
+    response.setHeader ('Content-Type', 'text/html');
+    const index = fs.readFileSync('./index.html');
+    const about = fs.readFileSync('./about.html');
+    const services = fs.readFileSync('./services.html');
+    
+    if (request.url === '/about') {
+        response.write(about);
+        return response.end();
+    }
+    else if (request.url === '/services') {
+        response.write(services);
+        return response.end();
+    } 
+    else if (request.url === '/' || request.url === '/index' ) {
+        response.write(index);
+        return response.end();
+    } else {
+        response.statusCode = 404;
+        response.write("Not found");
+        return response.end(); 
+    }
+});
+
+server.listen(3000, () => {
+    console.log('It is runing');
+
+}) 
---
+++ hw1/3.2/server_json.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const fs = require ('fs');
+
+
+const server = http.createServer(function(request, response) {
+    response.setHeader ('Content-Type', 'application/json');
+    
+    // response.write(`{"firstName" : "Pavel", "lastName" : "Ulasik"}`)
+
+    
+    const json = fs.readFileSync('./index.json');
+    
+    
+
+    response.write(json);
+   
+    response.end();
+});
+
+server.listen(3000, () => {
+    console.log('It is runing');
+
+}) 
