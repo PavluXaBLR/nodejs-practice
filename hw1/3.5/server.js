fd696f9376a1d71e40bbcf58fb4f95a5232b6087
PavluXaBLR <pavel.ulasik1984@gmail.com>
2021-12-16 17:46:50 +0300

    3.5 ready

--- hw1/3.5/server.js
+++ hw1/3.5/server.js
@@ -4,25 +4,8 @@ const fs = require ('fs');
 
 
 const server = http.createServer(function(request, response) {
-    response.setHeader ('Content-Type', 'text/html');
-    const index = fs.readFileSync('./index.html');
-    const about = fs.readFileSync('./about.html');
-    const services = fs.readFileSync('./services.html');
-
     
-    if (request.url === '/about') {
-        response.write(about);
-        return response.end();
-    }
-    else if (request.url === '/services') {
-        response.write(services);
-        return response.end();
-    } 
-    else if (request.url === '/' || request.url === '/index' ) {
-        response.write(index);
-        return response.end();
-    } 
-    else if (request.method === "POST" && request.url === '/post')  {
+    if (request.method === "POST" && request.url === '/post')  {
         response.setHeader ('Content-Type', 'application/json');
         response.write(`{"firstName" : "Pavel", "lastName" : "Ulasik"}`)
 
